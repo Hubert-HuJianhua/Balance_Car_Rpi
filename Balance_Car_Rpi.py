d2314b839cee71761428017cee6bee047b7f951d
小红旗 <hujianhua_8@live.com>
2020-08-02 22:13:43 +0800

    Gyro 测试

---
+++ Balance_Car_Rpi.py
@@ -0,0 +1,38 @@
+import smbus
+import time
+
+
+class Gyro(object):
+
+    def __init__(self, addr):
+        self.addr = addr
+        self.i2c = smbus.SMBus(1)
+
+    def get_angle(self):
+        try:
+            self.raw_angle_x = self.i2c.read_i2c_block_data(self.addr, 0x3d, 2)
+            self.raw_angle_y = self.i2c.read_i2c_block_data(self.addr, 0x3e, 2)
+            self.raw_angle_z = self.i2c.read_i2c_block_data(self.addr, 0x3f, 2)
+        except IOError:
+            print("ReadError: gyro_angle")
+            return (0, 0, 0)
+        else:
+            self.k_angle = 180
+
+            self.angle_x = (self.raw_angle_x[1] << 8 | self.raw_angle_x[0]) / 32768 * self.k_angle
+            self.angle_y = (self.raw_angle_y[1] << 8 | self.raw_angle_y[0]) / 32768 * self.k_angle
+            self.angle_z = (self.raw_angle_z[1] << 8 | self.raw_angle_z[0]) / 32768 * self.k_angle
+            if self.angle_x >= self.k_angle:
+                self.angle_x -= 2 * self.k_angle
+
+            if self.angle_y >= self.k_angle:
+                self.angle_y -= 2 * self.k_angle
+
+            if self.angle_z >= self.k_angle:
+                self.angle_z -= 2 * self.k_angle
+            return (self.angle_x, self.angle_y, self.angle_z)
+
+head_gyro = Gyro(0x50)
+while(True):
+        print(" Acc:  " + repr(head_gyro.get_acc()) + "\n", "Gyro: " + repr(head_gyro.get_gyro()) + "\n", "Angle:" + repr(head_gyro.get_angle()) + "\n")
+        time.sleep(0.2)
