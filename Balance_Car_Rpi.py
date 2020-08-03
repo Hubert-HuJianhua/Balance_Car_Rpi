88a2e69732d168877b1e076211f1f89f95e7cb32
小红旗 <hujianhua_8@live.com>
2020-08-03 15:42:22 +0800

    PID PD平衡环

--- Balance_Car_Rpi.py
+++ Balance_Car_Rpi.py
@@ -21,6 +21,31 @@ class Gyro(object):
         self.addr = addr
         self.i2c = smbus.SMBus(1)
 
+    def get_gyro(self):
+        try:
+            self.raw_gyro_x = self.i2c.read_i2c_block_data(self.addr, 0x37, 2)
+            self.raw_gyro_y = self.i2c.read_i2c_block_data(self.addr, 0x38, 2)
+            self.raw_gyro_z = self.i2c.read_i2c_block_data(self.addr, 0x39, 2)
+        except IOError:
+            print("ReadError: gyro_gyro")
+            return (0, 0, 0)
+        else:
+            self.k_gyro = 2000
+            self.gyro_x = (self.raw_gyro_x[1] << 8 | self.raw_gyro_x[0]) / 32768 * self.k_gyro
+            self.gyro_y = (self.raw_gyro_y[1] << 8 | self.raw_gyro_y[0]) / 32768 * self.k_gyro
+            self.gyro_z = (self.raw_gyro_z[1] << 8 | self.raw_gyro_z[0]) / 32768 * self.k_gyro
+
+            if self.gyro_x >= self.k_gyro:
+                self.gyro_x -= 2 * self.k_gyro
+
+            if self.gyro_y >= self.k_gyro:
+                self.gyro_y -= 2 * self.k_gyro
+
+            if self.gyro_z >= self.k_gyro:
+                self.gyro_z -= 2 * self.k_gyro
+
+            return (self.gyro_x, self.gyro_y, self.gyro_z)
+
     def get_angle(self):
         try:
             self.raw_angle_x = self.i2c.read_i2c_block_data(self.addr, 0x3d, 2)
@@ -47,6 +72,8 @@ class Gyro(object):
 
 def Motor(speed):
     if(speed>0):
+        if(speed>=100):
+            speed=100
         pwm.ChangeDutyCycle(speed)
         pwm2.ChangeDutyCycle(speed)
         GPIO.output(20, False)
@@ -54,6 +81,8 @@ def Motor(speed):
         GPIO.output(9, True)
         GPIO.output(11, False)
     elif{speed<0}:
+        if(speed<=-100):
+            speed=-100
         pwm.ChangeDutyCycle(-speed)
         pwm2.ChangeDutyCycle(-speed)
         GPIO.output(20, True)
@@ -70,10 +99,13 @@ def Motor(speed):
 pwm.start(90)
 pwm2.start(90)
 head_gyro = Gyro(0x50)
-p = 2.5
+p = 3.4
+d = 0.5
+midle_Value=2.3
 while(True):
         angle=head_gyro.get_angle()
-        Motor(angle[0]*p)
+        gyro=head_gyro.get_gyro()
+        Motor((midle_Value+angle[0])*p+gyro[0]*d)
         time.sleep(0.02)
 
 
